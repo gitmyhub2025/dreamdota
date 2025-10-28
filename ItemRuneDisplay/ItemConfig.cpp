41074e6efeb18969140dde0d835993497df4f4ff
Administrator <672209356@qq.com>
2025-10-28 22:17:33 +0800

    测试中文

--- ItemRuneDisplay/ItemConfig.cpp
+++ ItemRuneDisplay/ItemConfig.cpp
@@ -7,7 +7,7 @@
 //=============================================================================
 // 静态成员初始化
 //=============================================================================
-DebugMode ItemConfig::s_debugMode = DebugMode::ALL;  // 默认显示所有// 或 FILTERED, NONE
+DebugMode ItemConfig::s_debugMode = DebugMode::FILTERED;  // ALL 默认显示所有// 或 FILTERED, NONE
 std::map<std::string, std::string> ItemConfig::s_itemNames;
 std::set<std::string> ItemConfig::s_filterList;
 float ItemConfig::s_textTagSize = 0.024f;  // 调小字体大小
@@ -18,8 +18,9 @@ int ItemConfig::s_textTagColor[4] = {255, 204, 0, 255};  // 默认金黄色 (RGB
 //=============================================================================
 void ItemConfig::Initialize() {
 
-    s_itemNames["Q201"] = u8"速度之靴";
-    
+    s_itemNames["Q20I"] = u8"速度之靴";
+    s_itemNames["V30I"] = u8"敏捷便携";
+    s_itemNames["B30I"] = u8"力量手套";
 
     // 初始化过滤列表（在FILTERED模式下，只显示这些物品）
     // 默认包含所有有中文名称的物品
